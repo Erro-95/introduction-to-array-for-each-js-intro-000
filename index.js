9c73bb30433b3de83e040f7bbc1c3b8629e9a4ab
Edwin Rodriguez Ortiz <rodriguezedwin1324@gmail.com>
2020-07-02 00:26:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-function changeCompletely(element, index, array) {
-  array[index] = Math.floor(Math.random() * 100 + 2).toString() + ` ${array[index]}s!!!`;
+function doToElementsInArray(array, callback){
+  array.forEach(callback)
 }
- 
-var animals = ["dog", "cat", "squirrel"];
- 
-doToElementsInArray(animals, changeCompletely);
- 
-console.log(animals);
+
+function changeCompletely(array, )
